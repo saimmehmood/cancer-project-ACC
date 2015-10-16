a80dd9abe5f03fca488cac7f50c54fe9b97c0fde
saimmehmood <bsef12m557@pucit.edu.pk>
2015-10-16 21:06:32 +0200

    Changing last line.

--- flask_example.py
+++ flask_example.py
@@ -16,4 +16,4 @@ def index():
     return render_template('index.html', directory=find_items('output_folder'))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
