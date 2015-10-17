779c995e057663309d64abceadf5ddf561fad1d8
Jonas Samuelsson <jonas.samuelsson91@gmail.com>
2015-10-17 16:05:21 +0200

    now includes a celery worker

--- celery_tasks.py
+++ celery_tasks.py
@@ -1,17 +1,18 @@
-#from celery import Celery
+from celery import Celery
 #from subprocess import call
 import os
 
-#app = Celery('tasks', backend='amqp', broker='amqp://')
+app = Celery('celery_tasks', backend='amqp', broker='amqp://')
 
 #call(["ls", "-l"])
 
 # Remember to give the complete path to the folders
+@app.task(name='celery_tasks.run_CellProfiler', bind=True)
 def run_CellProfiler(inputFolder, outputFolder, pipeline):
 	cellProfiler = 'python ~/CellProfiler/CellProfiler.py'
 	os.system(cellProfiler + ' -c -i ' + inputFolder + ' -o ' + outputFolder + ' -p ' + pipeline)
+	return 0
 
 
 
-
-run_CellProfiler('~/TranslocationData', '~/output', '~/simpletransproject.cppipe')
+#run_CellProfiler('~/TranslocationData', '~/output', '~/simpletransproject.cppipe')
